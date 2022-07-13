5db36f81f19867e30a04c2c28b39548ba459aedf
Marcus Hudnell <103913070+hudnellmarcus@users.noreply.github.com>
2022-07-12 23:37:42 -0700

    photo blank=true

--- festivalist_app/models.py
+++ festivalist_app/models.py
@@ -83,7 +83,7 @@ class Festival(models.Model):
     days = models.IntegerField()
     venue = models.ForeignKey(Venue, default=None, on_delete=models.CASCADE)
     date = models.DateField('festival_date')
-    photo = models.ForeignKey(Photo, default=None, on_delete=models.CASCADE)
+    photo = models.ForeignKey(Photo, blank=True, on_delete=models.CASCADE)
     
     
     def __str__(self):
