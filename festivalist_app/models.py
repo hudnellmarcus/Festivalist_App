627dfeb8845c3a0077654862b68ef609ae3433c3
Marcus Hudnell <103913070+hudnellmarcus@users.noreply.github.com>
2022-07-14 00:41:24 -0700

    dropped and added database

--- festivalist_app/models.py
+++ festivalist_app/models.py
@@ -88,7 +88,7 @@ class Festival(models.Model):
     days = models.IntegerField()
     venue = models.ForeignKey(Venue, default=None, on_delete=models.CASCADE)
     date = models.DateField('festival_date')
-    # photo = models.ForeignKey(Photo, blank=True, on_delete=models.CASCADE)
+    photo = models.ForeignKey(Photo, blank=True, on_delete=models.CASCADE)
 
 
     
