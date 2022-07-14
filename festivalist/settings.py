95eac307036757b24e51bbc884bf4e0243b02204
Marcus Hudnell <103913070+hudnellmarcus@users.noreply.github.com>
2022-07-14 04:24:22 -0700

    added login redirect

--- festivalist/settings.py
+++ festivalist/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/festivals/saved'
 LOGOUT_REDIRECT_URL = '/'
 
 # Default primary key field type
